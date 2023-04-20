5762e3c482e49a36b932417cc602ddbe5ea8d560
Matt Sanford <matt.san21@gmail.com>
2023-04-19 21:40:52 -0400

    basic splash

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import WeddingTitle from './components/title.vue';
+const imgUrl = new URL('assets/ring.jpg', import.meta.url).href;
+</script>
+
+<template>
+  <div class="content">
+    <wedding-title />
+    <p>Save the date coming soon!</p>
+  </div>
+  <div class="background" :style="`background-image: url(${imgUrl});`"></div>
+</template>
+
+<style scoped lang="scss">
+.background {
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 20;
+  backdrop-filter: blur(3px);
+  transition: backdrop-filter 0.3s ease-in-out;
+  background-color: rgba(var(--background), 0.6);
+
+  p {
+    font-family: WeddingSubtitle;
+    font-weight: bolder;
+    font-size: 24px;
+    margin: 0px;
+    padding: 0px;
+    color: rgb(var(--text));
+  }
+}
+</style>
---
+++ src/components/title.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Heather Matt</h1>
+  <span>&#38;</span>
+</template>
+
+<style scoped lang="scss">
+  h1 {
+    box-sizing: border-box;
+    margin: 0px;
+    padding: 0px;
+    font-size: 72px;
+    font-family: WeddingTitle;
+    word-spacing: 30px;
+    color: rgba(var(--text), 0.93);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  span {
+    font-size: 128px;
+    margin: 0px;
+    padding: 0px;
+    color: rgb(var(--accentText));
+    position: absolute;
+    margin-top: -30px;
+    margin-left: 128px;
+    z-index: -10;
+    font-family: WeddingTitle;
+  }
+</style>
